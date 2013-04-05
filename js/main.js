98bf803fe2dbe5336d78733dbd86ad47ded6a6fd
jnagy <joel.nagy@iris-worldwide.com>
2013-04-05 17:36:02 -0400

    A little styling and an on screen trace console.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var $_ = $_ || []; // iris stuff
+$_.logging = 'Trace';
+
 // THE APP
 var app = {
 
@@ -10,7 +13,7 @@ var app = {
 
 	// RENDER HOME SCREEN
 	renderHomeView: function() {
-		$('body').html(this.homeTpl());
+		$('#Root').html(this.homeTpl());
 	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
 	},
 
@@ -25,6 +28,7 @@ var app = {
 
 	// INIT
     initialize: function() {
+$_.log('init...');
 		var self = this;
         this.store = new WebSqlStore(function() {
 			self.renderHomeView();
@@ -37,6 +41,21 @@ var app = {
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
+};
+
+$_.log = function(msg) {
+	if ($_.logging != false) {
+		try {
+			if (console != null && $_.logging != 'Trace')
+				console.log(msg);
+			else {
+				if ($('#Trace').length == 0) $('body').append('<div id="Trace" style="position: absolute; padding: 2px; background: yellow; color: black; bottom: 0; right: 0; z-index: 99999999999;"></div>');
+				s = $('#Trace').html();
+				if (s != '') s+='<br />';
+				$('#Trace').css({'display': 'block'}).html(s+msg);
+			}
+		} catch (e) { }
+	}
 };
 
 // Run once the device or browser/page is ready
